788f3316d1d49c0ed7e31e2d5aee11fd874f334a
Adarsh M <66694285+Adarsh-m-0@users.noreply.github.com>
2024-02-01 03:25:36 +0530

    02012024-Added features
    Added BMR based on activity level

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function calculateBMI() {
   const Age = document.getElementById("Age").value;
   const genderMale = document.getElementById("Male").checked;
   const genderFemale = document.getElementById("Female").checked;
+  const activityLevel = document.getElementById("activityLevel").value;
 
   if (!(genderMale || genderFemale)) {
     alert("Please select your gender.");
@@ -22,10 +23,37 @@ function calculateBMI() {
     bmr = 447.593 + 9.247 * weight + 3.098 * height - 4.33 * Age;
   }
 
+  
+
   const bmrIn = bmr.toFixed(2);
+
+  let adjustedBMR;
+  switch (activityLevel) {
+      case "sedentary":
+          adjustedBMR = bmr * 1.2;
+          break;
+      case "lightlyActive":
+          adjustedBMR = bmr * 1.375;
+          break;
+      case "moderatelyActive":
+          adjustedBMR = bmr * 1.55;
+          break;
+      case "veryActive":
+          adjustedBMR = bmr * 1.725;
+          break;
+      case "extremelyActive":
+          adjustedBMR = bmr * 1.9;
+          break;
+      default:
+          adjustedBMR = bmr; 
+  }
+
+  const adjustedBMRIn = adjustedBMR.toFixed(2);
 
   document.getElementById("result").innerHTML = `Your BMI is ${bmiIn}`;
   document.getElementById("result").innerHTML += `<br>Your BMR is ${bmrIn}`;
+  document.getElementById("result").innerHTML += `<br>Adjusted BMR based on activity: ${adjustedBMRIn}`;
+  
 
   if (bmiIn < 18.5) {
     document.getElementById("result").innerHTML +=
@@ -39,7 +67,27 @@ function calculateBMI() {
   } else {
     document.getElementById("result").innerHTML += "<br><br>You are obese.";
   }
+
+  const RecalUnder = adjustedBMRIn + 500;
+  const RecalOver = adjustedBMRIn - 500;
+
+  if(bmiIn < 18.5){
+    document.getElementById("result").innerHTML += `<br>Recommended calorie intake: ${RecalUnder}`;
+  }else if (bmiIn < 30) {
+    document.getElementById("result").innerHTML += `<br>Recommended calorie intake: ${RecalOver}`;
+  }else {
+
+    if(genderMale){
+        document.getElementById("result").innerHTML += `<br>Recommended calorie intake: 1500-1800`;
+    }else if (genderFemale) {
+        document.getElementById("result").innerHTML += `<br>Recommended calorie intake: 1200-1500 `;
+    }
+  }
+
+
 }
+
+
 
 //Dark mode
 
